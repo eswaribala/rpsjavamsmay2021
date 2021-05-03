8479e6381d1055e13ef9185a2c926c2471bc89f7
eswaribala <Parameswaribala@gmail.com>
2021-05-03 17:32:02 +0530

    Add files via upload

--- CustomerController.java
+++ CustomerController.java
@@ -6,13 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.boa.upload.models.Customer;
 import com.boa.upload.services.CustomerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.bohnman.squiggly.Squiggly;
+import com.github.bohnman.squiggly.util.SquigglyUtils;
 
 @RestController
 @RequestMapping("/customers")
@@ -38,6 +43,33 @@ public class CustomerController {
 		return this.customerService.getAllCustomers();
 	}
 	
+	//filtering
+	//http://localhost:7070/customers/v1.0/filters?fields=customerId,customerName,
+
+	@GetMapping({"/v1.0/filters", "/v1.1/filters"})
+    public String getFilteredCustomer(@RequestParam(name = "fields", required = false) 
+    String fields) 
+	{
+
+		List<Customer> customerList = getAllCustomers();
+		ObjectMapper mapper = Squiggly.init(new ObjectMapper(), fields);  
+		return SquigglyUtils.stringify(mapper, customerList);
+		
+    }
+	//filtering
+		//http://localhost:7070/customers/v1.0/filters?fields=customerId,customerName,
+
+		@GetMapping({"/v1.0/filters/{customerId}", "/v1.1/filters/{customerId}"})
+	    public String getFilteredCustomerById(@RequestParam(name = "fields", required = false) 
+	    String fields, @PathVariable("customerId") long customerId) 
+		{
+
+			Customer customerObj = this.customerService.getCustomerById(customerId);
+			ObjectMapper mapper = Squiggly.init(new ObjectMapper(), fields);  
+			return SquigglyUtils.stringify(mapper, customerObj);
+			
+	    }
+
 	
 
 }
